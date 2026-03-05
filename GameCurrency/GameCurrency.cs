eb401b0f4afa4100f9f1991d446be75c55b0487f
DESKTOP-NJGSL36\ok623 <hyunwoo.nam@digipen.edu>
2026-03-05 16:54:55 +0900

    작성중

---
+++ GameCurrency/GameCurrency.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+struct GameCurrency
+{
+    public int Gold;
+    public int Silver;
+
+    public GameCurrency(int Gold, int Silver)
+    {
+        this.Gold = Gold;
+        this.Silver = Silver;
+        if(this.Silver >= 100)
+        {
+            int temp = this.Silver / 100;
+            this.Silver -= 100 * temp;
+            this.Gold += temp;
+        }
+    }
+
+    public int GetTotalSilver()
+    {
+        return (Gold * 100) + Silver;
+    }
+
+    public static GameCurrency operator +(GameCurrency g1, GameCurrency g2)
+    {
+        return new GameCurrency(0, g1.GetTotalSilver() + g2.GetTotalSilver());
+    }
+
+    public static GameCurrency operator -(GameCurrency g1, GameCurrency g2)
+    {
+        int temp = g1.GetTotalSilver() - g2.GetTotalSilver();
+
+        if(temp < 0)
+        {
+            temp = 0;
+        }
+
+        return new GameCurrency(0, temp);
+    }
+
+    public static bool operator ==(GameCurrency g1, GameCurrency g2)
+    {
+        return g1.GetTotalSilver() == g2.GetTotalSilver();
+    }
+
+    public static bool operator !=(GameCurrency g1, GameCurrency g2)
+    {
+        return !(g1 == g2);
+    }
+
+    public static bool operator <(GameCurrency a, GameCurrency b)
+    {
+        return a.GetTotalSilver() < b.GetTotalSilver();
+    }
+
+    public static bool operator >(GameCurrency a, GameCurrency b)
+    {
+        return a.GetTotalSilver() > b.GetTotalSilver();
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is GameCurrency other)
+        {
+            return this == other;
+        }
+        return false;
+    }
+
+    public  bool Equals(GameCurrency other)
+    {
+        return this == other; ;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Gold, Silver);
+    }
+
+    public override string ToString()
+    {
+        return $"{Gold}G {Silver}S";
+    }
+
+}
---
+++ GameTime/GameTime.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+struct GameTime
+{
+    public int Hours;
+    public int Minutes;
+    public int Seconds;
+
+    public GameTime(int hours, int minutes, int seconds)
+    {
+        this.Hours = hours;
+        this.Minutes = minutes;
+        this.Seconds = seconds;
+
+        if (this.Seconds >= 60)
+        {
+            int temp = this.Seconds / 60;
+            this.Seconds -= 60 * temp;
+            this.Minutes += temp;
+        }
+
+        if (this.Minutes >= 60)
+        {
+            int temp = this.Minutes / 60;
+            this.Minutes -= 60 * temp;
+            this.Hours += temp;
+        }
+
+    }
+
+    public int GetTotalSeconds()
+    {
+        int m = (Hours * 60) + Minutes;
+        return (m * 60) + Seconds;
+    }
+
+    public static GameTime operator +(GameTime g1, GameTime g2)
+    {
+        return new GameTime(0,0,g1.GetTotalSeconds()+g2.GetTotalSeconds());
+    }
+
+    public static GameTime operator -(GameTime a, GameTime b)
+    {
+        int diff = a.GetTotalSeconds() - b.GetTotalSeconds();
+        return new GameTime(0, 0, Math.Max(0, diff));
+    }
+    public static GameTime operator *(GameTime a, int multiplier)
+    {
+        return new GameTime(0, 0, a.GetTotalSeconds() * multiplier);
+    }
+    public static bool operator ==(GameTime a, GameTime b)
+    {
+        return a.GetTotalSeconds() == b.GetTotalSeconds(); ;
+    }
+    public static bool operator !=(GameTime g1, GameTime g2)
+    {
+        return !(g1 == g2);
+    }
+
+    public static bool operator <(GameTime a, GameTime b)
+    {
+        return a.GetTotalSeconds() < b.GetTotalSeconds();
+    }
+
+    public static bool operator >(GameTime a, GameTime b)
+    {
+        return a.GetTotalSeconds() > b.GetTotalSeconds();
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is GameTime other)
+        {
+            return this == other;
+        }
+        return false;
+    }
+
+    public bool Equals(GameTime other)
+    {
+        return this == other; ;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Hours, Minutes, Seconds);
+    }
+
+    public override string ToString()
+    {
+        return $"{Hours}h {Minutes}m {Seconds}s";
+    }
+}
